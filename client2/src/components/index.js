119b07611a40e752975b0c9f7a994460dcc60ff6
prathviraj b <prathviraj.b.n@gmail.com>
2023-04-08 18:51:56 +0530

    Delete client2 directory

--- client2/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as NavBar } from './NavBar';
-export { default as SideBar } from './SideBar';
-export { default as CustomButton } from './CustomButton';
-export { default as FormFeild } from './FormFeild';
-export { default as DisplayCampaigns } from './DisplayCampaigns';
--- client2/src/pages/CampaignDetails.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Campaign = () => {
-  return (
-    <div>Campaign details</div>
-  )
-}
-
-export default Campaign;
