88b7f130dd0a252613df41d86e8ae1ab5438864c
rohitraman <rohitraman9944@gmail.com>
2023-05-08 10:49:14 +0530

    Update api.test.js

--- server/test/api.test.js
+++ server/test/api.test.js
@@ -15,9 +15,10 @@ describe("API Test Suite", () => {
       .post("/api/users/login")
       .send(loginData);
     jwtToken = response.body.jwt;
-    console.log("Test Before " + jwtToken);
     expect(response.status).toBe(200);
     expect(jwtToken).toBeDefined();
+    console.log(response);
+    console.log("Test Before " + jwtToken);
   }, 70000);
 
   test("Another API Using JWT Token", async () => {
