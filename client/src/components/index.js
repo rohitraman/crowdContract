0377d1e06753b94cef5394149a995341706b0e7c
prathviraj b <prathviraj.b.n@gmail.com>
2023-05-08 14:42:54 +0530

    Update index.js

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,3 +3,4 @@ export { default as SideBar } from './SideBar';
 export { default as CustomButton } from './CustomButton';
 export { default as FormFeild } from './FormFeild';
 export { default as DisplayCampaigns } from './DisplayCampaigns';
+export { default as CountBox } from './CountBox';
